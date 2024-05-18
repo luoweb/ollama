6ffef58fae4cd77f6c23a7766e20504416d2fea8
robin <hongxing525@126.com>
2024-05-18 15:32:53 +0800

    gcc and cmake config

--- scripts/rh_linux_deps.sh
+++ scripts/rh_linux_deps.sh
@@ -23,16 +23,16 @@ if grep -i "centos" /etc/system-release >/dev/null; then
     # yum install -y devtoolset-9-gcc devtoolset-9-gcc-c++ wget
     ls -l /opt/rh/
     # scl enable devtoolset-8 bash
-    rm -rf /usr/bin/cmake
-    wget https://cmake.org/files/v3.22/cmake-3.22.1-linux-x86_64.sh && chmod +x *.sh && \
-    ./cmake-3.22.1-linux-x86_64.sh --skip-license --prefix=/usr/local/
-    ln -s /usr/local/bin/cmake /usr/bin/cmake
-    ln -s /opt/rh/devtoolset-8/root/bin/gcc /usr/bin/gcc
-    ln -s /opt/rh/devtoolset-8/root/bin/g++ /usr/bin/g++
-    cmake --version
-    gcc --version
-    g++ --version
-    command -v g++ gcc cmake
+    # rm -rf /usr/bin/cmake
+    # wget https://cmake.org/files/v3.22/cmake-3.22.1-linux-x86_64.sh && chmod +x *.sh && \
+    # ./cmake-3.22.1-linux-x86_64.sh --skip-license --prefix=/usr/local/
+    # ln -s /usr/local/bin/cmake /usr/bin/cmake
+    # ln -s /opt/rh/devtoolset-8/root/bin/gcc /usr/bin/gcc
+    # ln -s /opt/rh/devtoolset-8/root/bin/g++ /usr/bin/g++
+    # cmake --version
+    # gcc --version
+    # g++ --version
+    # command -v g++ gcc cmake
 elif grep -i "rocky" /etc/system-release >/dev/null; then
     # Temporary workaround until rocky 8 AppStream ships GCC 10.4 (10.3 is incompatible with NVCC)
     cat << EOF > /etc/yum.repos.d/Rocky-Vault.repo

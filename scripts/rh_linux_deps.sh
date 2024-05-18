22be51f0437857478db6d38f8a955e92eca7e1fe
robin <hongxing525@126.com>
2024-05-19 00:11:48 +0800

    fix(build): custom build

--- scripts/rh_linux_deps.sh
+++ scripts/rh_linux_deps.sh
@@ -10,7 +10,7 @@ if grep -i "centos" /etc/system-release >/dev/null; then
     # uninstall and ignore failures
     yum remove -y git
     yum -y install epel-release centos-release-scl
-    yum -y install dnf wget
+    yum -y install dnf wget sed
     
     if [ "${MACHINE}" = "x86_64" ]; then
         yum -y install https://repo.ius.io/ius-release-el7.rpm
